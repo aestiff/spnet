f04218af09c3992721f1d1a48e16453599b39fd9
stiff <a.e.stiff@gmail.com>
2015-05-28 21:25:03 -0400

    Basic code clean-up. Compiles.

--- spnet.cpp
+++ spnet.cpp
@@ -32,6 +32,10 @@ float	v[N], u[N];				// activity variables
 int	N_firings;				// the number of fired neurons 
 const   int         N_firings_max=100*N;	// upper limit on the number of fired neurons per sec
 int	firings[N_firings_max][2];              // indeces and timings of spikes
+double	C_max=10;		
+const	int	W=3;	                        // initial width of polychronous groups
+int     min_group_path = 7;		        // minimal length of a group
+int	min_group_time = 40;	                // minimal duration of a group (ms)
 
 void initialize()
 {
@@ -137,6 +141,235 @@ void initialize()
   firings[0][0]=-D;	// put a dummy spike at -D for simulation efficiency 
   firings[0][1]=0;	// index of the dummy spike  
 }
+
+const	int	latency = D; // maximum latency 
+//--------------------------------------------------------------
+int			N_polychronous;
+
+
+double		C_rel = 0.95*C_max;
+const int	polylenmax = N;
+int			N_postspikes[polylenmax], I_postspikes[polylenmax][N], J_postspikes[polylenmax][N], D_postspikes[polylenmax][N], L_postspikes[polylenmax][N];
+double		C_postspikes[polylenmax][N];
+int			N_links, links[2*W*polylenmax][4];
+int			group[polylenmax], t_fired[polylenmax], layer[polylenmax];
+int			gr3[W], tf3[W];
+int			I_my_pre[3*M], D_my_pre[3*M], N_my_pre;
+int			N_fired;
+
+
+FILE		*fpoly;
+
+
+//--------------------------------------------------------------
+void	polychronous(int nnum){
+  int	i,j, t, p, k;
+  int npre[W];
+  int dd;
+  int	t_last, timing;
+  int	Dmax, L_max; 
+  int	used[W], discard;
+  
+  double v[N],u[N],I[N];
+  
+  
+  N_my_pre = 0;
+  for (i=0;i<N_pre[nnum];i++){
+    if (*s_pre[nnum][i] > C_rel) {
+	I_my_pre[N_my_pre]=I_pre[nnum][i];
+	D_my_pre[N_my_pre]=D_pre[nnum][i];
+	N_my_pre++;
+    }
+  }
+  if (N_my_pre<W){
+    return;
+  }
+  for (i=0;i<W;i++){
+    npre[i]=i;
+  }
+  
+  while (0==0){
+    Dmax=0;
+    for (i=0;i<W;i++){
+      if (Dmax < D_my_pre[npre[i]]){
+	Dmax=D_my_pre[npre[i]];
+      }
+    }
+    for (i=0;i<W;i++)	{
+      group[i]=I_my_pre[npre[i]];
+      t_fired[i]= Dmax-D_my_pre[npre[i]];
+      layer[i]=1;
+			
+      for (dd=0; dd<D; dd++){		 
+	for (j=0; j<delays_length[group[i]][dd]; j++) {
+	  p = post[group[i]][delays[group[i]][dd][j]];
+	  if ((s[group[i]][delays[group[i]][dd][j]] > C_rel) & (dd>=D_my_pre[npre[i]])) {
+	    timing = t_fired[i]+dd+1;
+	    J_postspikes[timing][N_postspikes[timing]]=group[i];				// presynaptic
+	    D_postspikes[timing][N_postspikes[timing]]=dd;					// delay
+	    C_postspikes[timing][N_postspikes[timing]]=s[group[i]][delays[group[i]][dd][j]];	// syn weight
+	    I_postspikes[timing][N_postspikes[timing]++]=p;					// index of post target	
+	  }
+	}
+      }
+    }
+
+    for (i=0;i<N;i++){
+      v[i]=-70;
+      u[i]=0.2*v[i];
+      I[i]=0;
+    }
+
+    N_links = 0;
+    N_fired=W;
+    t_last = D+D+latency+1;
+    t=-1;
+    while ((++t<t_last) & (N_fired < polylenmax)){
+      for (p=0;p<N_postspikes[t];p++){ 
+	I[I_postspikes[t][p]]+=C_postspikes[t][p]; 
+      }
+      for (i=0;i<N;i++) {
+	v[i]+=0.5*((0.04*v[i]+5)*v[i]+140-u[i]+I[i]);
+	v[i]+=0.5*((0.04*v[i]+5)*v[i]+140-u[i]+I[i]);
+	u[i]+=a[i]*(0.2*v[i]-u[i]);
+	I[i]=0;
+      }
+
+      for (i=0;i<N;i++){ 
+	if (v[i]>=30) {
+	  v[i] = -65;
+	  u[i]+=d[i];
+
+	  if (N_fired < polylenmax){
+	    t_fired[N_fired]= t;
+	    group[N_fired++]=i;
+	    for (dd=0; dd<D; dd++){
+	      for (j=0; j<delays_length[i][dd]; j++){
+		if ((s[i][delays[i][dd][j]] > C_rel) | (i>=Ne)) {
+		  timing = t+dd+1;
+		  J_postspikes[timing][N_postspikes[timing]]=i;				// presynaptic
+		  D_postspikes[timing][N_postspikes[timing]]=dd;				// delay
+		  //L_postspikes[timing][N_postspikes[timing]]=NL+1;			// layer
+		  C_postspikes[timing][N_postspikes[timing]]=s[i][delays[i][dd][j]];	   // syn weight
+		  I_postspikes[timing][N_postspikes[timing]++]=post[i][delays[i][dd][j]];// index of post target	
+		}
+	      }
+	    }
+	    if (t_last < timing+1) {
+	      t_last = timing+1;
+	      if (t_last > polylenmax-D-1){
+		t_last = polylenmax-D-1;
+	      }
+	    }
+	  }
+	}
+      }
+		
+      if (N_fired>2*W){
+	  N_links=0;
+	  L_max=0;
+	  for (i=W;i<N_fired;i++){
+	    layer[i]=0;
+	    for (p=t_fired[i]; (p>t_fired[i]-latency) & (p>=0); p--){
+	      for (j=0;j<N_postspikes[p];j++){
+		if ((I_postspikes[p][j]==group[i]) & (J_postspikes[p][j]<Ne)) {
+		  for (k=0;k<i;k++){
+		    if ((group[k]==J_postspikes[p][j]) & (layer[k]+1>layer[i])){
+		      layer[i]=layer[k]+1;
+		    }
+		  }
+		  //{   
+		  links[N_links][0]=J_postspikes[p][j];
+		  links[N_links][1]=I_postspikes[p][j];
+		  links[N_links][2]=D_postspikes[p][j];
+		  links[N_links++][3]=layer[i];
+		  if (L_max < layer[i]){
+		    L_max = layer[i];
+		  }
+		  //}
+		}
+	      }
+	    }
+	  }
+										 
+	  discard = 0;
+	  for (i=0;i<W;i++) {
+	    used[i]=0;
+	    for (j=0;j<N_links;j++){
+	      if ((links[j][0] == group[i]) & (links[j][1] < Ne)){
+		used[i]++;
+	      }
+	    }
+	    if (used[i] == 1) {
+	      discard = 1;
+	    }
+	  }
+
+	  //if ((discard == 0) & (t_fired[N_fired-1] > min_group_time) )  // (L_max >= min_group_path))
+	  if ((discard == 0) & (L_max >= min_group_path)) {
+
+	    for (i=0;i<W;i++) {
+	      gr3[i]=group[i];
+	      tf3[i]=t_fired[i];
+	    } //???
+
+	    N_polychronous++;
+	    cout << "\ni= " << nnum
+		 << ", N_polychronous= " << N_polychronous
+		 << ", N_fired = " << N_fired
+		 << ", L_max = " << L_max
+		 << ", T=" << t_fired[N_fired-1];
+	    
+	    //fprintf(fpoly, " %d  %d,       ", N_fired, L_max);
+	    //for (i=0; i<N_fired; i++)
+	    //fprintf(fpoly, " %d %d, ", group[i], t_fired[i]);
+	    //fprintf(fpoly, "        ");
+	    //for (j=0;j<N_links;j++){
+	    //   fprintf(fpoly, " %d %d %d %d,  ", links[j][0], links[j][1], links[j][2], links[j][3]);
+	    //}
+	    //fprintf(fpoly, "\n");
+	  }
+      }
+
+      for (dd=Dmax;dd<t_last;dd++) {
+	N_postspikes[dd]=0;
+      }
+      if (t_last == polylenmax-D) {
+	for (dd=t_last;dd<polylenmax;dd++) {
+	  N_postspikes[dd]=0;
+	}
+      }
+      i=1;
+      while (++npre[W-i] > N_my_pre-i){
+	if (++i > W){
+	  return;
+	}
+      }
+      while (i>1) {
+	npre[W-i+1]=npre[W-i]+1; i--;
+      }
+    }	
+  }
+}  
+
+ 
+//--------------------------------------------------------------
+void	all_polychronous()
+{
+  int	i;
+  N_polychronous=0;
+  fpoly = fopen("..//polyall.dat","w"); //necessary??
+  for (i=0;i<polylenmax;i++) N_postspikes[i]=0;
+  
+  for (i=0;i<Ne;i++) polychronous(i);
+  
+  cout << "\nN_polychronous=" << N_polychronous << "\n";
+  fclose(fpoly);
+}
+
+
+
+
 int main(int argc, char *argv[]){
   int		i, j, k, sec, t;
   float	I[N];
